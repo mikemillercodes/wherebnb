a35e53068f63ab5a35151d740e9ced8d7355fd7d
Mike Miller <millermichaelg1@gmail.com>
2022-10-22 18:11:10 -0500

    Updated file structure

---
+++ backend/db/seeders/20221020213257-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return queryInterface.bulkInsert('Users', [
+    {
+      username: 'Demo-lition',
+      email: 'demo@user.io',
+      firstName: 'Demo',
+      lastName: 'UserLName',
+      hashedPassword: bcrypt.hashSync('password')
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition'] }
+    })
+  }
+};
