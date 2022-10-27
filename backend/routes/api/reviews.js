eb6552ce4989d67dcb78c057c5806b6110f5cfb8
Mike Miller <millermichaelg1@gmail.com>
2022-10-27 17:18:41 -0500

    Spotid added to review groupBy

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -58,7 +58,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
         where: {
             userId
         },
-        group: ['Review.id', 'User.id', 'ReviewImages.id']
+        group: ['Review.id', 'User.id', 'ReviewImages.id', 'Spot.id']
     })
 
     const result = []
