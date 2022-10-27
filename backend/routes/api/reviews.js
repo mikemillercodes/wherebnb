0961b019edba92be2d8e499e0617472601f557c8
Mike Miller <millermichaelg1@gmail.com>
2022-10-27 17:17:36 -0500

    updated group reviewImages

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -58,7 +58,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
         where: {
             userId
         },
-        group: ['Review.id', 'User.id']
+        group: ['Review.id', 'User.id', 'ReviewImages.id']
     })
 
     const result = []
