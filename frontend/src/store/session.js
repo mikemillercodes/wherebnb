17b7206e7d19fa0baeeee5fb50402a0a94baf5b1
Mike Miller <millermichaelg1@gmail.com>
2022-11-14 12:16:02 -0600

    logout action complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -49,3 +49,35 @@ const sessionReducer = (state = initialState, action) => {
 };
 
 export default sessionReducer;
+
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
